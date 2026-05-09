91f3329428672a0508ac4f37883c3f29440aedac
G1anC <noah.steiniger@epitech.eu>
2026-05-09 19:47:59 +0200

    feat(icons): add home, close, plus, notification, arrow, dashboard, folder

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,10 +1,17 @@
 export const icons = {
-  collapse:  'solar:layers-bold-duotone',
-  search:    'solar:magnifer-bold-duotone',
-  settings:  'solar:settings-bold-duotone',
-  edit:      'solar:pen-new-square-bold-duotone',
-  remove:    'solar:trash-bin-2-bold-duotone',
-  calendar:  'solar:calendar-add-line-bold-duotone',
+  collapse:      'solar:layers-bold-duotone',
+  search:        'solar:magnifer-bold-duotone',
+  settings:      'solar:settings-bold-duotone',
+  edit:          'solar:pen-new-square-bold-duotone',
+  remove:        'solar:trash-bin-2-bold-duotone',
+  calendar:      'solar:calendar-add-line-bold-duotone',
+  home:          'solar:home-2-bold-duotone',
+  close:         'solar:close-circle-bold-duotone',
+  plus:          'solar:add-circle-bold-duotone',
+  notification:  'solar:bell-bold-duotone',
+  arrow:         'solar:alt-arrow-right-bold-duotone',
+  dashboard:     'solar:qr-code-bold-duotone',
+  folder:        'solar:folder-open-bold-duotone',
 } as const;
 
 export type IconKey = keyof typeof icons;
